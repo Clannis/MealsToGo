1a09c79f50884f3b2fb843c451233469d57805b5
Clannis <Dodds1990@gmail.com>
2021-08-21 18:50:40 -0400

    Add default restaurant info

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { Text } from "react-native";
 
-export const RestaurantInfo = () => {
-  return <Text>Restaurant Info</Text>;
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://images.all-free-download.com/images/graphiclarge/food_picture_03_hd_pictures_167556.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return <Text>{name}</Text>;
 };
