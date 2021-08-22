68c3a12d0b37f1b34eb63e03e201677ed466b20d
James <Dodds1990@gmail.com>
2021-08-22 15:10:02 -0400

    Add px to safge area styling to remove error

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -3,5 +3,5 @@ import { SafeAreaView, StatusBar } from "react-native";
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}`}
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
 `;
