b7a1f6937405c3a6a1567aff556053ce0de0d324
Clannis <Dodds1990@gmail.com>
2021-08-22 11:56:00 -0400

    Install camelize and camilize response data from mock restaurant api call

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,19 +1,26 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
 
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
-  return new Promise((res, rej) => {
+  return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
-      rej("not found");
+      reject("not found");
     }
-    res(mock);
+    resolve(mock);
   });
 };
 
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
+
 restaurantsRequest()
-  .then((result) => {
-    console.log(result);
+  .then(restaurantsTransform)
+  .then((transformedResponse) => {
+    console.log(transformedResponse);
   })
   .catch((err) => {
-    console.log(err);
+    console.log("error");
   });
