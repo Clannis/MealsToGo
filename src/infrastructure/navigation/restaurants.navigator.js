99520f8565a53e2999c236b10527830facd57d51
Clannis <Dodds1990@gmail.com>
2021-08-24 17:48:00 -0400

    Add functionality to navigate to restaurant detail stack screen on restaurant list item press. Remove headers from screen. Add es lint spelling

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "react-native";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
@@ -7,10 +8,14 @@ const RestaurantStack = createNativeStackNavigator();
 
 export const RestaurantNavigator = () => {
   return (
-    <RestaurantStack.Navigator>
+    <RestaurantStack.Navigator screenOptions={{ headerShown: false }}>
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={() => <Text>Restaurant Detail</Text>}
       />
     </RestaurantStack.Navigator>
   );
