f76ed9dd42714c3f8dd15b6c3af3b4192bb58891
Clannis <Dodds1990@gmail.com>
2021-09-01 09:32:59 -0400

    add background color to safeareaviews

--- src/components/utility/safe-area.component.js
+++ src/components/utility/safe-area.component.js
@@ -4,8 +4,10 @@ import { SafeAreaView, StatusBar } from "react-native";
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 export const SafeAreaNoMargin = styled(SafeAreaView)`
   flex: 1;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
