678cd7c1bf56cfca6d22d43a29a72ebe8d60b836
Clannis <Dodds1990@gmail.com>
2021-08-31 21:58:20 -0400

    Link map callout press to restaurant detail screen

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -12,7 +12,7 @@ const Map = styled(MapView)`
   width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
   const [latDelta, setLatDelta] = useState(0);
@@ -47,7 +47,11 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
               }}
             >
-              <MapView.Callout>
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", { restaurant })
+                }
+              >
                 <MapCallout restaurant={restaurant} />
               </MapView.Callout>
             </MapView.Marker>
