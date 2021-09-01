cb23ccd1db7fa539f8ace9b04b94cec10dc64e3c
Clannis <Dodds1990@gmail.com>
2021-09-01 01:54:08 -0400

    Create authentication service and context imported at app level

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import { Navigation } from "./src/infrastructure/navigation/index";
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavoritesContextProvider } from "./src/services/favorites/favorites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA2wZmxaxbYlKXRXcN-hFtAWgCJh_WPlCI",
@@ -32,22 +33,6 @@ if (!firebase.apps.length) {
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    setTimeout(
-      () =>
-        firebase
-          .auth()
-          .signInWithEmailAndPassword("test@test.com", "test123")
-          .then((user) => {
-            setIsAuthenticated(true);
-          })
-          .catch((e) => {
-            console.log(e);
-          }),
-      2000
-    );
-  }, []);
-
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -60,18 +45,18 @@ export default function App() {
     return null;
   }
 
-  if (!isAuthenticated) return null;
-
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavoritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavoritesContextProvider>
+        <AuthenticationContextProvider>
+          <FavoritesContextProvider>
+            <LocationContextProvider>
+              <RestaurantsContextProvider>
+                <Navigation />
+              </RestaurantsContextProvider>
+            </LocationContextProvider>
+          </FavoritesContextProvider>
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
